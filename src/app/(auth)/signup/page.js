438ad735a557b05faed5e2959abb82a097c7e3bf
Amirali Amanipour <arshia.ala80@gmail.com>
2023-09-06 12:32:09 +0330

    worked on the front of the signup page

---
+++ src/app/(auth)/signup/page.js
@@ -0,0 +1,11 @@
+import SignUpPage from "@/components/templates/SignUpPage";
+
+export default function page() {
+
+
+    return (
+        <div>
+            <SignUpPage />
+        </div>
+    )
+}
